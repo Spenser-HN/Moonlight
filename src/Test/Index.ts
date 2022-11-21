c19bf03f064111c5aa091ab22a18d6fcfe075760
Spenser-HN <107572202+Spenser-HN@users.noreply.github.com>
2022-11-21 03:57:47 -0600

    This file is just for testing purposes

---
+++ src/Test/Index.ts
@@ -0,0 +1,34 @@
+interface NewOne {
+    Data: string,
+}
+
+const FirstData: NewOne = {
+    Data: "This is my first data"
+}
+
+type int = number;
+type int8 = number;
+type int16 = number;
+type int32 = number;
+type int64 = number;
+
+type float = number;
+type float32 = number;
+type float64 = number;
+
+type char = string;
+type byte = string;
+
+
+const Num: int64 = 5;
+
+
+const NewFunction = () => { }
+
+
+class Avion {
+    constructor(parameters) {
+
+    }
+}
+
