6210cb9544d76ccb3a22316c309106dd2ed7e4b0
Spenser-HN <107572202+Spenser-HN@users.noreply.github.com>
2022-11-21 15:29:03 -0600

    add file extension checking

--- src/FileReader/ReadFile.go
+++ src/FileReader/ReadFile.go
@@ -1,5 +1,10 @@
 package filereader
 
+import (
+	"log"
+	"strings"
+)
+
 type ReadedFile map[int8]map[int32]string
 
 func ReadFile(Files_Path []string) (ReadedFile, []string) {
@@ -7,6 +12,10 @@ func ReadFile(Files_Path []string) (ReadedFile, []string) {
 	var Readed ReadedFile = make(ReadedFile)
 
 	for read := 0; read < len(Files_Path); read++ {
+
+		if strings.HasSuffix(Files_Path[read], ".ts") == false {
+			log.Fatal(Files_Path[read], " no es un archivo de Typescript")
+		}
 
 		//Reading file by file
 		Readed[int8(read)] = ReadLines(Files_Path[read])
